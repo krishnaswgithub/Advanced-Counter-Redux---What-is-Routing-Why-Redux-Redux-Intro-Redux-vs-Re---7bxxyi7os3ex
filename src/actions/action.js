8c21422eeef680d6c8eeb18297e15c3b01937c8c
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-17 19:43:42 +0530

    Update action.js

---
+++ src/actions/action.js
@@ -0,0 +1,30 @@
+export const increment = () => {
+    return {
+      type: 'INCREMENT',
+    };
+  };
+  
+  export const decrement = () => {
+    return {
+      type: 'DECREMENT',
+    };
+  };
+  
+  export const signIn = () => {
+    return {
+      type: 'SIGN_IN',
+    };
+  };
+  
+  export const signOut = () => {
+    return {
+      type: 'SIGN_OUT',
+    };
+  };
+  
+  export const incrementByAmount = (amount) => {
+    return {
+      type: 'INCREMENTBYAMOUNT',
+      payload: amount,
+    };
+  };
